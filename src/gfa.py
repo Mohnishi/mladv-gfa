f8d627cc84930f36c41426fddc21163d8c3db4d4
Arvid Fahlström Myrman <behold@behold.se>
2016-12-28 16:45:13 +0100

    Add GFA skeleton

---
+++ src/gfa.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+
+class GFA:
+
+    def __init__(self, rank=4, factors=7, max_iter=100, lamb=0.1):
+        self.lamb = lamb
+        self.rank = rank
+        self.factors = factors
+
+    def fit(self, X, D):
+        # D = np.array([...])
+        self.groups = len(D)
+        self.U = np.random.normal(loc=0, scale=np.sqrt(1/self.lamb),
+                                  size=(self.groups, self.rank))
+        self.V = np.random.normal(loc=0, scale=np.sqrt(1/self.lamb),
+                                  size=(self.factors, self.rank))
+        self.mu_u = np.zeros((self.groups, 1))
+        self.mu_v = np.zeros((self.factors, 1))
+
+    def update_W(self):
+        pass
+
+    def update_Z(self):
+        pass
+
+    def update_alpha(self):
+        pass
+
+    def update_tau(self):
+        pass
