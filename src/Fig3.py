17592de9df52c28fb413f05c2f5cf12ba3a150ab
langenb <langenb@kth.se>
2017-01-03 21:21:08 +0100

    vizualize artificial data

--- src/Fig3.py
+++ src/Fig3.py
@@ -7,20 +7,13 @@ import numpy as np
 from generate_data import *
 
 
+# Example how to use :
+R = 3 #rank
+K = 7 #factors
+Dm = [10,10,10] #groups
+N = 100 #samples
+X, W, Z = generation(N, K, Dm, R)
 
-
-
-# # Example how to use :
-# R = 3 #rank
-# K = 7 #factors
-# Dm = [10,10,10] #groups
-# N = 100 #samples
-# X, W, Z = generation(N, K, Dm, R)
-
-
-# print(W.shape)
-
-W = np.ones((7, 30))
 
 X1 = W[:,range(0,10)]
 X2 = W[:,range(10,20)]
@@ -42,7 +35,6 @@ plt.ylabel("Group 1")
 
 plt.figure(2)
 
-
 for k in K:
 	for d in Dm:
 		plt.scatter(k+1,d+1,s=X2[k,d],color='black', marker = 's')
@@ -60,7 +52,6 @@ for k in K:
 plt.title("True")		
 
 plt.ylabel("Group 3")
-
 
 plt.show()
 
