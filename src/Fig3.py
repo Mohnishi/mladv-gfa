d9671ef55a510833eee80bcad8a6d0574ced4ae6
langenb <langenb@kth.se>
2017-01-04 21:51:46 +0100

    scaling problem not solved yet

--- src/Fig3.py
+++ src/Fig3.py
@@ -2,58 +2,54 @@ import importlib
 import gfa
 importlib.reload(gfa)
 import matplotlib.pyplot as plt
-
 import numpy as np
 from generate_data import *
 
 
-# Example how to use :
+# Create Corentin's artifical data set corresponding to Fig. 3 (original paper)
 R = 3 #rank
 K = 7 #factors
-Dm = [10,10,10] #groups
+D = np.array([10,10,10]) #groups     np.array([2,4,4])
 N = 100 #samples
-X, W, Z = generation(N, K, Dm, R)
+X, W, Z = generation(N, K, D, R)
 
 
-X1 = W[:,range(0,10)]
-X2 = W[:,range(10,20)]
-X3 = W[:,range(20,30)]
 
-K = range(7) 
-Dm = range(10)
+# Extract groups of the true projection mappings 
+W1 = W[:,range(0,10)]
+W2 = W[:,range(10,20)]
+W3 = W[:,range(20,30)]
 
 
+
+# Run GFA 
+g = gfa.GFA(optimize_method="bfgs", debug=True)
+g.fit(X,D)
+
+# Get estimated W (West)
+West = g.m_W
+# Extract groups of estimated projection mappings
+West1 = g.m_W[0]
+West2 = g.m_W[1]
+West3 = g.m_W[2]
+
+
+# Visualize the group 1 of the true projection mappings 
 plt.figure(1)
-
-for k in K:
-	for d in Dm:
-		plt.scatter(k+1,d+1,s=X1[k,d],color='black', marker = 's')
+for k in range(7):
+	for d in range(10):
+		plt.scatter(k+1,d+1,s=W1[k,d],color='black', marker = 's')
 	
-plt.title("True")		
-
+plt.title("True W")		
 plt.ylabel("Group 1")
 
+# Visualize the group 1 of the estimated projection mappings
 plt.figure(2)
-
-for k in K:
-	for d in Dm:
-		plt.scatter(k+1,d+1,s=X2[k,d],color='black', marker = 's')
+for k in range(7):
+	for d in range(10):
+		plt.scatter(k+1,d+1,s=West1[k,d],color='black', marker = 's')
 	
-plt.title("True")		
-
-plt.ylabel("Group 2")
-
-plt.figure(3)
-
-for k in K:
-	for d in Dm:
-		plt.scatter(k+1,d+1,s=X3[k,d],color='black', marker = 's')
-	
-plt.title("True")		
-
-plt.ylabel("Group 3")
+plt.title("Estimated W")		
+plt.ylabel("Group 1")
 
 plt.show()
-
-
-
