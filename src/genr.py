4ae1c5653ddad00dd993812115e11fb8b6d2f55f
axelri <axel.riese@gmail.com>
2017-01-06 16:52:23 +0100

    generalize R transformation

--- src/genr.py
+++ src/genr.py
@@ -4,34 +4,45 @@
 import numpy as np
 from generate_data import *
 
+def print_to_R(X, D, R, K):
+    X_mean = X.mean(axis=1, keepdims=True)
+    X = X - X_mean
+
+    # groups
+    M = len(D)
+    # samples
+    N = X.shape[1]
+    # transpose to author's format and split into 3 groups
+    Y = np.hsplit(X.T, M)
+
+    # generate R code to stdout
+    ys = []
+    for i in range(M):
+        print("y{} <- matrix(c({}), nrow={}, ncol={},byrow=TRUE)".format(
+            i+1, ",".join(map(str, Y[i].flatten().tolist())), N, D[i]))
+        ys.append("y"+str(i+1))
+
+    print("Y <- list({})".format(",".join(ys)))
+    print("K <- {}".format(K))
+    print("R <- {}".format(R))
+
+    # calling in R:
+    # source("CCAGFA.R")
+    # opts <- getDefaultOpts()
+    # opts$R <- R
+    # opts$dropK <- FALSE
+
+    # dropK FALSE means the code won't drop small factors,
+    # hence keep going even if it doesn't find correlations
+
 # Create Corentin's artifical data set corresponding to Fig. 3 (original paper)
 R = 3 #rank
 K = 7 #factors
-D = np.array([10,10,10]) #groups     np.array([2,4,4])
+D = np.array([10,10,10,10]) #groups     np.array([2,4,4])
 N = 100 #samples
 X, W, Z = generation(N, K, D, R)
 
 # centering of data must be done beforehand
 X_mean = X.mean(axis=1, keepdims=True)
 X = X - X_mean
-
-# transpose to author's format and split into 3 groups
-Y = np.hsplit(X.T, 3)
-
-# generate R code to stdout
-for i in range(len(Y)):
-    print("y{} <- matrix(c({}), nrow={}, ncol={},byrow=TRUE)".format(
-        i+1, ",".join(map(str, Y[i].flatten().tolist())), N, D[i]))
-
-print("Y <- list(y1, y2, y3)")
-print("K <- {}".format(K))
-print("R <- {}".format(R))
-
-# calling in R:
-# source("CCAGFA.R")
-# opts <- getDefaultOpts()
-# opts$R <- R
-# opts$dropK <- FALSE
-
-# dropK FALSE means the code won't drop small factors,
-# hence keep going even if it doesn't find correlations
+print_to_R(X, D, R, K)
