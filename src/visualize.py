dc95aefd95b621ac8f75762f7c1115b16ff3dc05
Jean Martin <nitram.naej2@gmail.com>
2017-01-10 10:25:04 +0100

    Sort W by testing every permutation.

--- src/visualize.py
+++ src/visualize.py
@@ -7,7 +7,7 @@ def plot_W(W):
                 s=abs(W).flatten(), color='black', marker='s')
 
 
-def sort_W(W_real, W_est):
+def sort_W_old(W_real, W_est):
     K = W_real.shape[0]
     sim = np.zeros([K, K])
     for i in range(K):
@@ -21,3 +21,43 @@ def sort_W(W_real, W_est):
             sim[index_maxi[0], i] = 0
             sim[i,index_maxi[1]] = 0
     return W_est[list_max,:]
+    
+    
+    
+def sort_W(W_real, W_est):   
+    K = W_real.shape[0]
+    f = np.sum(W_est, axis = 1) == 0
+    P = K - np.sum(f)
+    sim = np.zeros([P,K])
+    
+    # Matrix of similarity
+    W_tmp = W_est[np.sum(W_est, axis=1) != 0]    
+    for i in range(P):
+        for j in range(K):
+            sim[i,j] = np.dot(np.abs(W_tmp[i,:]), np.abs(W_real[j,:])) / (np.linalg.norm(W_tmp[i,:]) * np.linalg.norm(W_real[j,:]))
+    
+    # Compute the similairty for every permutation 
+    a = list(iter.permutations(range(K)))
+    maxi_score = 0
+    maxi_permu = a[0]
+    for permu_all in a:
+        permu = permu_all[0:P]
+        score = 0
+        for i in range(P):
+            score += sim[i, permu[i]]
+        if score > maxi_score:
+            maxi_score = score
+            maxi_permu = permu_all
+    # 
+    final_permu = np.asarray(maxi_permu)
+    offset_zero = 0
+    offset = 0
+    for i in range(len(final_permu)):
+        if f[i]:
+            final_permu[i] = maxi_permu[offset_zero + P]
+            offset_zero += 1
+        else:
+            final_permu[i] = maxi_permu[offset]
+            offset += 1
+    return W_est[np.argsort(final_permu), :]
+    
