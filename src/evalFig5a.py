723855e93c44a3bc82e17c9e8094a021383d9e1b
Jean Martin <nitram.naej2@gmail.com>
2017-01-15 16:06:42 +0100

    Better plot

--- src/evalFig5a.py
+++ src/evalFig5a.py
@@ -4,31 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-datasetindex = int(sys.argv[1])
-rstart = int(sys.argv[2])
-rend = int(sys.argv[3])
-rstep = int(sys.argv[4])
-num_datasets = int(sys.argv[5])
+rstart = int(sys.argv[1])
+rend = int(sys.argv[2])
+rstep = int(sys.argv[3])
+num_datasets = int(sys.argv[4])
 
 
-RMSE = np.load('Fig5a-numdatasets{}-datasetindex{}-modelranks{}-{}-{}-yaxis.npy'.format(
-    num_datasets, datasetindex, rstart, rend, rstep))
-NRMSE = np.load('Fig5a-numdatasets{}-datasetindex{}-modelranks{}-{}-{}-yaxis-N.npy'.format(
-    num_datasets, datasetindex, rstart, rend, rstep))
-NSRMSE = np.load('Fig5a-numdatasets{}-datasetindex{}-modelranks{}-{}-{}-yaxis-NS.npy'.format(
-    num_datasets, datasetindex, rstart, rend, rstep))
-R_array = np.load('Fig5a-numdatasets{}-datasetindex{}-modelranks{}-{}-{}-xaxis.npy'.format(
-    num_datasets, datasetindex, rstart, rend, rstep))
+RMSE_6 = np.load('Fig5a-numdatasets{}-datasetindex1-modelranks{}-{}-{}-yaxis-NS.npy'.format(
+    num_datasets, rstart, rend, rstep))
+RMSE_10 = np.load('Fig5a-numdatasets{}-datasetindex2-modelranks{}-{}-{}-yaxis-NS.npy'.format(
+    num_datasets, rstart, rend, rstep))
+R_array_6 = np.load('Fig5a-numdatasets{}-datasetindex1-modelranks{}-{}-{}-xaxis.npy'.format(
+    num_datasets, rstart, rend, rstep))
+R_array_10 = np.load('Fig5a-numdatasets{}-datasetindex2-modelranks{}-{}-{}-xaxis.npy'.format(
+    num_datasets, rstart, rend, rstep))
 
+correct_rank_6 = np.argwhere(R_array_6 == 6)[0]
+correct_rank_10 = np.argwhere(R_array_10 == 10)[0]
 
 plt.figure(1)
-plt.plot(R_array, RMSE)
-plt.show()
-
-plt.figure(2)
-plt.plot(R_array, NRMSE)
-plt.show()
-
-plt.figure(2)
-plt.plot(R_array, NSRMSE)
+plt1, = plt.plot(R_array_6, RMSE_6, '-o', color='r')
+plt2, = plt.plot(R_array_10, RMSE_10, '-o', color='g')
+plt.scatter(R_array_6[correct_rank_6], RMSE_6[correct_rank_6], s=1, c='b')
+plt.scatter(R_array_6[correct_rank_6], RMSE_6[correct_rank_6], s=500, c='w')
+plt.scatter(R_array_10[correct_rank_10], RMSE_10[correct_rank_10], s=1, c='b')
+plt.scatter(R_array_10[correct_rank_10], RMSE_10[correct_rank_10], s=500, c='w')
+plt.legend((plt1, plt2), ('Data rank 6', 'Data rank 10'),loc = 'upper right', fontsize='20')
+plt.ylabel('Prediction RMSE', fontsize='20')
+plt.xlabel('Model rank', fontsize='20')
 plt.show()
